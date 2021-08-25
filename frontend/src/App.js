a5a0561467bc0b7509732365b6787eba0b5dfcbd
Ayush Gupta <ayushgupta0010@gmail.com>
2021-08-25 14:53:26 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+const App = () => (
+  <BrowserRouter>
+    <Switch></Switch>
+  </BrowserRouter>
+);
+export default App;
