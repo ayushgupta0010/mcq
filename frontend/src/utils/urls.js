2323dd45906157a0bf4eba2bd99f60081b2b833e
Ayush Gupta <ayushgupta0010@gmail.com>
2021-08-25 23:41:40 +0530

    Fixed urls

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -6,16 +6,16 @@ export const WEBSITE = {
 export const USER = {
   LOGIN_URL: WEBSITE.BACKEND_URL + "user/login",
   SIGNUP_URL: WEBSITE.BACKEND_URL + "user/signup",
-  REFRESH_TOKEN_URL: WEBSITE.BACKEND_URL + "auth/refresh_token",
+  REFRESH_TOKEN_URL: WEBSITE.BACKEND_URL + "user/refresh_token",
 };
 
 export const QUESTION = {
-  CREATE_URL: WEBSITE.BACKEND_URL + "question/create",
-  LIST_BY_URL: WEBSITE.BACKEND_URL + "question/list/by/:username",
-  LIST_FOR_URL: WEBSITE.BACKEND_URL + "question/list/for/:username",
+  CREATE_URL: WEBSITE.BACKEND_URL + "mcq/question/create",
+  LIST_BY_URL: WEBSITE.BACKEND_URL + "mcq/question/list/by/:username",
+  LIST_FOR_URL: WEBSITE.BACKEND_URL + "mcq/question/list/for/:username",
 };
 
 export const ANSWER = {
-  CREATE_URL: WEBSITE.BACKEND_URL + "answer/create",
-  LIST_URL: WEBSITE.BACKEND_URL + "answer/list/:username",
+  CREATE_URL: WEBSITE.BACKEND_URL + "mcq/answer/create",
+  LIST_URL: WEBSITE.BACKEND_URL + "mcq/answer/list/:username",
 };
