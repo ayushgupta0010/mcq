2142770fcb4322f5c309c3633b7ef49f440a9631
Ayush Gupta <ayushgupta0010@gmail.com>
2021-08-25 16:22:05 +0530

    Added urls

--- backend/mcq/urls.py
+++ backend/mcq/urls.py
@@ -1,1 +1,12 @@
-urlpatterns = []
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('question/create', views.QuestionCreateView.as_view()),
+    path('question/list/by/<username>', views.QuestionListByView.as_view()),
+    path('question/list/for/<username>', views.QuestionListForView.as_view()),
+
+    path('answer/create', views.AnswerCreateView.as_view()),
+    path('answer/list/<username>', views.AnswerListView.as_view()),
+]
