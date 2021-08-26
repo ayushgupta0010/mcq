2cab0114c8be5a551b0231ba2ff599fa18968317
Ayush Gupta <ayushgupta0010@gmail.com>
2021-08-26 20:04:23 +0530

    Added Forbidden component

--- frontend/src/components/UtilityComponents/Forbidden.js
+++ frontend/src/components/UtilityComponents/Forbidden.js
@@ -1,7 +1,68 @@
 import React from "react";
 
-const Forbidden = () => {
-  return <div>Forbidden</div>;
-};
+const Forbidden = () => (
+  <div className='container'>
+    <div class='maincontainer'>
+      <div class='bat'>
+        <img
+          class='wing leftwing'
+          src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
+          alt=''
+        />
+        <img
+          class='body'
+          src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png'
+          alt='bat'
+        />
+        <img
+          class='wing rightwing'
+          src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
+          alt=''
+        />
+      </div>
+      <div class='bat'>
+        <img
+          class='wing leftwing'
+          src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
+          alt=''
+        />
+        <img
+          class='body'
+          src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png'
+          alt='bat'
+        />
+        <img
+          class='wing rightwing'
+          src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
+          alt=''
+        />
+      </div>
+      <div class='bat'>
+        <img
+          class='wing leftwing'
+          src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
+          alt=''
+        />
+        <img
+          class='body'
+          src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png'
+          alt='bat'
+        />
+        <img
+          class='wing rightwing'
+          src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
+          alt=''
+        />
+      </div>
+      <img
+        class='foregroundimg'
+        src='https://www.blissfullemon.com/wp-content/uploads/2018/09/HauntedHouseForeground.png'
+        alt='haunted house'
+      />
+    </div>
+    <h1 class='errorcode'>ERROR 403</h1>
+    <div class='errortext'>This area is forbidden. Turn back now!</div>
+  </div>
+);
 
 export default Forbidden;
