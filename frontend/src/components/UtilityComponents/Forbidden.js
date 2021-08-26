5b67062b9408d1498445ba34754334c5428002e1
Ayush Gupta <ayushgupta0010@gmail.com>
2021-08-27 00:54:00 +0530

    Changed class to className

--- frontend/src/components/UtilityComponents/Forbidden.js
+++ frontend/src/components/UtilityComponents/Forbidden.js
@@ -1,67 +1,65 @@
-import React from "react";
-
 const Forbidden = () => (
   <div className='container'>
-    <div class='maincontainer'>
-      <div class='bat'>
+    <div className='maincontainer'>
+      <div className='bat'>
         <img
-          class='wing leftwing'
+          className='wing leftwing'
           src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
           alt=''
         />
         <img
-          class='body'
+          className='body'
           src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png'
           alt='bat'
         />
         <img
-          class='wing rightwing'
+          className='wing rightwing'
           src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
           alt=''
         />
       </div>
-      <div class='bat'>
+      <div className='bat'>
         <img
-          class='wing leftwing'
+          className='wing leftwing'
           src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
           alt=''
         />
         <img
-          class='body'
+          className='body'
           src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png'
           alt='bat'
         />
         <img
-          class='wing rightwing'
+          className='wing rightwing'
           src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
           alt=''
         />
       </div>
-      <div class='bat'>
+      <div className='bat'>
         <img
-          class='wing leftwing'
+          className='wing leftwing'
           src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
           alt=''
         />
         <img
-          class='body'
+          className='body'
           src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png'
           alt='bat'
         />
         <img
-          class='wing rightwing'
+          className='wing rightwing'
           src='https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png'
           alt=''
         />
       </div>
       <img
-        class='foregroundimg'
+        className='foregroundimg'
         src='https://www.blissfullemon.com/wp-content/uploads/2018/09/HauntedHouseForeground.png'
         alt='haunted house'
       />
     </div>
-    <h1 class='errorcode'>ERROR 403</h1>
-    <div class='errortext'>This area is forbidden. Turn back now!</div>
+    <h1 className='errorcode'>ERROR 403</h1>
+    <div className='errortext'>This area is forbidden. Turn back now!</div>
   </div>
 );
 
