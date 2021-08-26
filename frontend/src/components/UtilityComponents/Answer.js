585d0cefff59d22ba8cd94d2510307b4b5ae20a4
Ayush Gupta <ayushgupta0010@gmail.com>
2021-08-26 16:51:07 +0530

    Fixed answer bug

--- frontend/src/components/UtilityComponents/Answer.js
+++ frontend/src/components/UtilityComponents/Answer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Question from "./Question";
 
+const getAnswer = (ans) =>
+  ans.replaceAll('"', "").replace("[", "").replace("]", "");
+
 const Answer = ({ answer }) => (
   <>
     <Question question={answer.question} />
     <div className='container'>
       <p className='text-skyblue m-0 mb-1'>
-        My answer: <span className='text-light'>{answer.answer}</span>
+        My answer:
+        <span className='text-light ms-1'>{getAnswer(answer.answer)}</span>
       </p>
       <p className='text-skyblue'>
         Marks: <span className='text-light'>{answer.marks}</span>
