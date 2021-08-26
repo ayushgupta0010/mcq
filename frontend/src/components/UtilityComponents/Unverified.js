02a01dc5eae7c08685d506c5f86a0991a614c6cb
Ayush Gupta <ayushgupta0010@gmail.com>
2021-08-26 20:49:27 +0530

    Added Unverified component

--- frontend/src/components/UtilityComponents/Unverified.js
+++ frontend/src/components/UtilityComponents/Unverified.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const Unverified = () => {
-  return <div>Unverified</div>;
+  const { isLoggedIn, is_verified } = useSelector((state) => state.auth);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!isLoggedIn) history.push("/login");
+    else if (is_verified === true) history.push("/");
+    else document.title = "Account Unverified";
+  }, [history, isLoggedIn, is_verified]);
+
+  return (
+    <div className='container'>
+      <div
+        className='d-flex justify-content-center align-items-center'
+        style={{ height: "85vh" }}>
+        <div className='text-center'>
+          <p className='text-skyblue m-0 fs-3'>
+            You have signed up as a teacher
+          </p>
+          <p className='text-skyblue m-0 fs-3'>
+            so until you are successfully verified as a teacher,
+          </p>
+          <p className='text-skyblue m-0 fs-3'>you will be shown this page</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Unverified;
