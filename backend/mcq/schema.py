7fa3f8f56c715b7901dbdd83f19edb18c98f376e
Ayush Gupta <ayushgupta0010@gmail.com>
2021-09-01 20:06:29 +0530

    Added mcq schema

--- backend/mcq/schema.py
+++ backend/mcq/schema.py
@@ -1,9 +1,94 @@
 import graphene
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from graphene_django import DjangoObjectType
+
+from .models import Question, Answer
+
+User = get_user_model()
+
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = '__all__'
 
 
 class Query(graphene.ObjectType):
-    pass
+    que_list_by_user = graphene.List(QuestionType, username=graphene.String(required=True))
+    que_list_for_user = graphene.List(QuestionType, username=graphene.String(required=True))
+    ans_list = graphene.List(AnswerType, username=graphene.String(required=True))
+
+    def resolve_que_list_by_user(root, info, username):
+        user = User.objects.get(username=username)
+        questions = user.questions.all().order_by('-timestamp')
+        return questions
+
+    def resolve_que_list_for_user(root, info, username):
+        user = User.objects.get(username=username)
+        que_ans_by_user = list(user.answers.values_list('question__id', flat=True))
+        questions = Question.objects.filter(~Q(id__in=que_ans_by_user))
+        return questions
+
+    def resolve_ans_list(root, info, username):
+        user = User.objects.get(username=username)
+        answers = user.answers.all().order_by('-timestamp')
+        return answers
+
+
+class QuestionInput(graphene.InputObjectType):
+    user = graphene.String(required=True)
+    question_type = graphene.String(required=True)
+    question = graphene.String(required=True)
+    mcqOptionA = graphene.String()
+    mcqOptionB = graphene.String()
+    mcqOptionC = graphene.String()
+    mcqOptionD = graphene.String()
+    correct_answer = graphene.String(required=True)
+
+
+class QuestionCreateMutation(graphene.Mutation):
+    class Arguments:
+        question_data = QuestionInput(required=True)
+
+    question = graphene.Field(QuestionType)
+
+    @classmethod
+    def mutate(cls, root, info, question_data):
+        question_data['user'] = User.objects.get(username=question_data['user'])
+        question = Question.objects.create(**question_data)
+        question.save()
+        return QuestionCreateMutation(question=question)
+
+
+class AnswerInput(graphene.InputObjectType):
+    user = graphene.String(required=True)
+    question = graphene.ID(required=True)
+    answer = graphene.String(required=True)
+    marks = graphene.Int(required=True)
+
+
+class AnswerCreateMutation(graphene.Mutation):
+    class Arguments:
+        ans_data = AnswerInput(required=True)
+
+    answer = graphene.Field(AnswerType)
+
+    @classmethod
+    def mutate(cls, root, info, ans_data):
+        ans_data['user'] = User.objects.get(username=ans_data['user'])
+        ans_data['question'] = Question.objects.get(id=ans_data['question'])
+        answer = Answer.objects.create(**ans_data)
+        answer.save()
+        return AnswerCreateMutation(answer=answer)
 
 
 class Mutation(graphene.ObjectType):
-    pass
+    create_que = QuestionCreateMutation.Field()
+    create_ans = AnswerCreateMutation.Field()
